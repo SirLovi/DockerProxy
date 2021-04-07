dc9104e6ac8196a9ccb9cc7431c8e1584ae724e4
SirLovi <stanekdo@gmail.com>
2021-04-07 16:35:13 +0200

    Adding keys file

--- src/index.py
+++ src/index.py
@@ -1,11 +1,41 @@
 import sys
+import os
+import tornado
 import tornado.web
 import tornado.ioloop
+from urllib.parse import urlparse
+
+################ UTILITY FUNCTIONS ################
+"""
+def getProxyKey(url):
+    urlParsed = urlparse(url, scheme="http")
+    proxyKey = "%s_proxy" % urlParsed.scheme
+    return os.environ.get(proxyKey)
+
+def getHostPort(url):
+    urlParsed = urlparse(url, scheme="http")
+    return urlParsed.hostname, urlParsed.port
+
+def fetch_request(url, callback, **args):
+    proxyKey = getProxyKey(url)
+    if proxyKey:
+        print("Forward request via upstream proxy " + proxyKey)
+        tornado.httpclient.AsyncHTTPClient.configure(
+            "tornado.curl_httpclient.CurlAsyncHTTPClient")
+        host, port = getHostPort(proxyKey)
+        args["proxy_host"] = host
+        args["proxy_port"] = port
+
+    req = tornado.httpclient.HTTPRequest(url, **args)
+    client = tornado.httpclient.AsyncHTTPClient()
+    client.fetch(req, callback, raise_error=False)
+"""
 
 ################ EXAMPLE TORNADO HANDLERS ################
 
 class basicRequestHandler(tornado.web.RequestHandler):
     def get(self):
+        #self.set_header("Acces-Control-Allow-Origin", "*")
         self.write("Hello, world")
 
 class staticRequestHandler(tornado.web.RequestHandler):
@@ -26,6 +56,9 @@ class resourceRequestRequestHandler(tornado.web.RequestHandler):
 ################ PROXY HANDLER ################
 
 class proxyHandler(tornado.web.RequestHandler):
+    SUPPORTED_METHODS = ("GET", "POST", "PUT", "DELETE")
+    # TODO Handle all these methods
+
     def get(self):
         print("Handling " + self.request.method + " request to " + self.request.uri)
 
@@ -34,22 +67,22 @@ class proxyHandler(tornado.web.RequestHandler):
     def post(self):
         return self.get()
 
-    def connect(self):
-        print("Handling CONNECT to " + self.request.uri)
-        host, port = self.request.uri.split(':')
-        client = self.request.connection.stream
+    def put(self):
+        print("Handling " + self.request.method + " request to " + self.request.uri)
 
-        # TODO Add client read/write/connect/close functions
+    def delete(self):
+        print("Handling " + self.request.method + " request to " + self.request.uri)
 
 
 ################ MAIN ################
 
 if __name__ == '__main__':
     app = tornado.web.Application([
-        # (r"/", basicRequestHandler),
-        # (r"/blog", staticRequestHandler),
-        # (r"/isEven", queryStringRequestHandler),
-        # (r"/tweet/([0-9]+)", resourceRequestRequestHandler),
+        (r"/tasks/", basicRequestHandler),
+        (r"/", basicRequestHandler),
+        (r"/blog", staticRequestHandler),
+        (r"/isEven", queryStringRequestHandler),
+        (r"/tweet/([0-9]+)", resourceRequestRequestHandler),
         (r".*", proxyHandler)
     ])
 
